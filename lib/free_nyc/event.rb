035b4244923e54e5bbab8de2abde0faaef83c451
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-06-26 19:04:12 +0000

    Stub scrape method with the first title

--- lib/free_nyc/event.rb
+++ lib/free_nyc/event.rb
@@ -1,3 +1,9 @@
 class Free_NYC::Event
 
+  attr_accessor :title, :borough, :location, :time, :description, :url
+
+  def initialize(title)
+    @title = title
+  end
+  
 end
--- lib/free_nyc/scraper.rb
+++ lib/free_nyc/scraper.rb
@@ -1,3 +1,10 @@
 class Free_NYC::Scraper
 
+  def scrape
+    doc = Nokogiri::HTML(open("https://www.nycgovparks.org/events"))
+
+    title = doc.css("div#events_leftcol a")[0].text
+    Free_NYC::Event.new(title)
+  end
+
 end
