1070cecbe433254eced19ea4949765beac16b733
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-06-26 18:48:48 +0000

    Fix stub CLI method functionality. Rename subdirectories in lib

--- lib/free_nyc/cli.rb
+++ lib/free_nyc/cli.rb
@@ -1,3 +1,5 @@
+require_relative "../free-nyc.rb"
+
 class Free_NYC::CLI
 
   def call
@@ -7,10 +9,12 @@ class Free_NYC::CLI
   def start
     puts "Welcome to Free in NYC!"
     input = gets.strip
-    case input
+    case input.to_i
     when 1
       puts "hello!"
     end
   end
 
 end
+
+Free_NYC::CLI.new.call
